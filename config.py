016523b52c49fd9abac8901d7d4747be073cfb03
Sarthak Sethi <sarthaksethi12@gmail.com>
2023-05-10 23:29:47 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+password = '1234'                # e.g. password = '1234'
+database_name = 'database_1A'    # e.g. database_name = 'database_1A'
+edit_mode_password = 'edit'      # e.g. edit_mode_password = 'edit'
+dr_mls_access_code = 'auth'      # e.g. dr_mls_access_code = 'auth'
